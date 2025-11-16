804954f7a39e77a0cbf3516e02ea001c3e1ff816
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-11-16 13:08:20 +0300

    create task N1, 2, 3, 4, 5

---
+++ main.py
@@ -0,0 +1,64 @@
+import asyncio
+from random import randint as rint
+
+
+# ДЗ — Корутины и asyncio
+# Задача №1
+async def say_hello(n):
+    await asyncio.sleep(n)
+    print('Привет!')
+
+
+# Задача №2
+async def delayed_print(text, delay):
+    await asyncio.sleep(delay)
+    print(text)
+
+
+# Задача №3
+async def task(delay):
+    await asyncio.sleep(delay)
+    print(f'Корутина N{delay}')
+
+
+def create_list_coroutine(coroutine, n):
+    list_corut = [coroutine(i) for i in range(1, n)]
+    return list_corut
+
+
+# Задача №4
+async def countdown(n):
+    if isinstance(n, int) and n >= 1:
+        print(f'Вывод чисел от {n} до 1 с интервалом в 1 сек.')
+        for i in range(n, 0, -1):
+            await asyncio.sleep(1)
+            print(i)
+    else:
+        print('Значение n должно быть >= 1')
+        return
+
+
+# Задача №5
+async def fetch_data(index):
+    delay = rint(1, 5)
+    print(f'Запущена задача №{index} с паузой {delay}сек.')
+    await asyncio.sleep(delay)
+    print(f'Данные задачи №{index} с паузой {delay}сек. получены')
+
+
+async def coroutine_func(index):
+    return int(input(f'Введите число №{index} для суммирования >> '))
+
+
+async def main():
+    await say_hello(1)
+    await delayed_print('Начало программы', 2)
+    await delayed_print('Окончание программы', 3)
+    await asyncio.gather(*create_list_coroutine(task, 4))
+    await countdown(13)
+    a, b, c = await asyncio.gather(*create_list_coroutine(coroutine_func, 4))
+    print(a + b + c)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
